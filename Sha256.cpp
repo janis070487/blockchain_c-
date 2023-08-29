8747b4a3e07c97ea5c18669cdffe00e4109ea5cd
janis070487 <janis070487@gmail.com>
2023-08-30 00:24:41 +0300

    test

--- Sha256.cpp
+++ Sha256.cpp
@@ -1,15 +1,23 @@
 #include "Sha256.h"
 #include <iostream>
-
+#define uint unsigned int
 using namespace std;
 
 void Sha256::Init(char data[], uint size) {
 	if (size < 56) { this->howBlockNead = 1; }
 	else {
 		this->howBlockNead = size / 64;
-		int atlikums = size % 64;
+		uint atlikums = size % 64;
 		if (atlikums < 56) { howBlockNead++; }
-		else { howBlockNead += 2;}
+		else { howBlockNead += 2; }
 	}
-	cout << howBlockNead << endl;
+	this->Data = new uint[16 * this->howBlockNead];
+	for (int i = 0; i < (16 * this->howBlockNead); i++) {
+		//Data[i] = 0;
+	};
+	cout << Data[4] << endl;
+	//for (int i = 0; i < 64; i++)
+	//{
+	//	cout << this->Data[i] << endl;
+	//}
 }
